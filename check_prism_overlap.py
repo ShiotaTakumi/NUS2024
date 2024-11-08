17c569a6e3aed97c85133e8a330374be5851efcc
ShiotaTakumi <shiotatakumi1009@outlook.jp>
2024-11-08 16:47:36 +0800

    Add base script for Python project

---
+++ check_prism_overlap.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python
+
+"""
+@file prism_overlap.py
+@brief 
+@detail 
+@version 1.0.0
+@date 2024-11-08
+@author Takumi Shiota
+"""
+
+__version__ = "1.0.0"
+__author__ = "Takumi Shiota"
+__date__ = "2024-11-08"
+
+def main():
+    print(f"Hello world!")
+
+
+####################
+if __name__ == "__main__":
+    main()
